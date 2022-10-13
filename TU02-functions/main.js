1660ba9882d9458b0b419c04b343f98856d3e4ba
Xavi Martinez <xavi12p@hotmail.com>
2022-10-13 21:01:39 +0200

    primera clase

---
+++ TU02-functions/main.js
@@ -0,0 +1,15 @@
+// Programa Principal
+seCreaLaPantalla();
+creoBotones();
+
+// Funciones Generales
+function seCreaLaPantalla() {
+  creaCabecera();
+  creaBody();
+  suma(2, 3);
+}
+
+// Funciones Específicas
+function creaCabecera() {}
+
+function creaBody() {}
