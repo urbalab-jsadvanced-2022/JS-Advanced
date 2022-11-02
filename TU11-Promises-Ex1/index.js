cff5ed9b6fd0005bc07cf2ee16d4092728f4e5e6
Xavi Martinez <xavi12p@hotmail.com>
2022-11-02 20:50:50 +0100

    Promises Ex1

--- TU11-Promises-Ex1/index.js
+++ TU11-Promises-Ex1/index.js
@@ -12,4 +12,6 @@ const span = document.createElement('span');
 span.textContent = 'Loading';
 document.body.append(span);
 
-delay(3000).then(() => (span.textContent = 'Completed'));
+delay(3000).then(() => {
+  span.textContent = 'Completed';
+});
