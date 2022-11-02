30230ab41d5797c0056e3f2a958f0f3ab850e0b2
Xavi Martinez <xavi12p@hotmail.com>
2022-11-02 20:49:35 +0100

    Promises Ex1

---
+++ TU11-Promises-Ex1/index.js
@@ -0,0 +1,15 @@
+// Código Callback en forma de Promesa
+function delay(ms) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+}
+
+// Crear SPAN
+const span = document.createElement('span');
+span.textContent = 'Loading';
+document.body.append(span);
+
+delay(3000).then(() => (span.textContent = 'Completed'));
