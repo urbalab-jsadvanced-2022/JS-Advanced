09b823753f76bc8ac201b02046bfdd62eaee2845
Xavi Martinez <xavi12p@hotmail.com>
2022-11-02 19:05:53 +0100

    Promises

--- TU11-Promises/withPromise.js
+++ TU11-Promises/withPromise.js
@@ -19,31 +19,7 @@ loadScript(
   './primero.js',
   // SEGUNDO ARGUMENTO - La funcion a ejecutar cuando el script se haya cargado
   () => {
-    loadScript(
-      // 1er
-      './segundo.js',
-      // 2o
-      () => {
-        // El script segundo.js se ha cargado
-        loadScript(
-          // 1er
-          './tercero.js',
-          // 2o
-          () => {
-            // El script tercero.js se ha cargado
-          },
-          // 3o
-          () => {
-            // El script tercero.js ha fallado
-            // QUE HACES???
-          }
-        );
-      },
-      // 3er
-      () => {
-        // El script segundo.js ha fallado
-      }
-    );
+    console.log('Todo ha ido bien');
   },
   // TERCER ARGUMENTO - La funcion a ejecutar SI la carga del script falla
   () => {
